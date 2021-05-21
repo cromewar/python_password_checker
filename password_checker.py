8b1711a9ba0855a7b45a23a2dbd7ffebd5fa0da7
Cromewar <vasiliy@cromewar.com>
2021-05-21 15:39:50 -0500

    adding file I/O support

--- password_checker.py
+++ password_checker.py
@@ -4,6 +4,11 @@ import sys
 
 
 def resquest_data(query_data):
+    '''
+    Takes data from the API provided by https://haveibeenpwned.com/
+        The query_data is a Hash code of 5 characters
+        returns the resoponse with all the data realated to the query_data
+        '''
     api_url = 'https://api.pwnedpasswords.com/range/' + query_data
     res = requests.get(api_url)
     if res.status_code != 200:
@@ -11,6 +16,13 @@ def resquest_data(query_data):
     return res
 
 def get_pass_leaks(hashes, hash_to_check):
+    '''
+    Takes two arguments: hashes what is the main part of the entire has code and hash_to_check
+    that reamins in the computer, this allows to not sent the entire password to the API and compare it's
+    existance locally
+    Funcionallity: Slipts each linea of the response given by request_data and returns how many times that password
+    has been found on any data leak
+    '''
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
         if h == hash_to_check:
@@ -28,15 +40,19 @@ def pass_check(password):
     return get_pass_leaks(response, remain)
 
 
-def main(args):
-    for password in args:
+def main():
+    '''reads the password file and then checks if there is or not a match, if the count is more than 0
+        it recommends to change the password.
+    '''
+    with open('password.txt', 'r') as password_file:
+        password = password_file.read()
         count = pass_check(password)
         if count:
-            print(f'The password {password} was founf {count} times, it\'s highly recommended to change your password')
+            print(f'The password {password} was found {count} times, it\'s highly recommended to change your password')
         else:
             print(f'the password {password} was NOT found, you have a solid one!')
         return 'done!'
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
 
